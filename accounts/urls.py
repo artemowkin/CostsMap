5e1ccc334009d06f5432f1ba1caaa297ea84eb01
artemowkin <artyom.groshev2017@gmail.com>
2020-09-08 16:31:56 +0700

    Fixed csrf token cache on login and signup page

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,8 +7,8 @@ from .views import SignupWithCategoriesView
 
 
 urlpatterns = [
-    path('signup/', cache_page(604800)(SignupWithCategoriesView.as_view()),
+    path('signup/', SignupWithCategoriesView.as_view(),
          name='account_signup'),
-    path('login/', cache_page(604800)(views.login), name='account_login'),
+    path('login/', views.login, name='account_login'),
     path('logout/', cache_page(604800)(views.logout), name='account_logout'),
 ]
