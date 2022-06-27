6eb1434968c4e154f2a6242974a36838e4ce0431
artemowkin <artyom.groshev2017@gmail.com>
2022-06-27 21:40:55 +0700

    Deleted react client (will be written on vue js in future)

--- client/src/hooks/Auth/useAuth.js
+++
@@ -1,8 +0,0 @@
-export const useAuth = () => {
-    let tokenValue = localStorage.getItem("tokenValue", null);
-    const tokenExpDate = localStorage.getItem("tokenExpDate", null);
-
-    tokenValue = (tokenExpDate !== null && Date.now() / 1000 >= tokenExpDate) ? "" : tokenValue;
-
-    return { tokenValue, tokenExpDate };
-}
--- client/src/utils/numbers.js
+++
@@ -1,6 +0,0 @@
-export const roundDecimal = (decimal) => {
-    if (isNaN(decimal))
-        return decimal
-
-    return Math.round(decimal * 100) / 100
-}
